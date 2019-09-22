77bd577ccc23c426c247eb3e22aefebf18abebd5
Kelly Manahan <kelly.a.manahan@gmail.com>
2019-09-22 14:50:47 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,21 @@
+def using_push(array, string)
+  array.push(string)
+end
+
+def using_unshift(array= ["Frida Kahlo", "Pablo Picasso", "Cai Guo-Qiang"], string = "hello")
+  puts array.unshift(string)
+  return array.unshift(string)
+end 
+
+#using_unshift
+
+def using_pop(array)
+  return array.pop  
+end
+
+def pop_with_args(array= ["Frida Kahlo", "Pablo Picasso", "Cai Guo-Qiang"])
+  puts array.pop[-2]
+  return array.pop[-2]
+end
+
+pop_with_args
