770df3d9f4fc9475f7df52e4c76906eca7286924
Kelly Manahan <kelly.a.manahan@gmail.com>
2019-09-22 14:51:30 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -13,9 +13,8 @@ def using_pop(array)
   return array.pop  
 end
 
-def pop_with_args(array= ["Frida Kahlo", "Pablo Picasso", "Cai Guo-Qiang"])
+def pop_with_args(array)
   puts array.pop[-2]
   return array.pop[-2]
 end
 
-pop_with_args
